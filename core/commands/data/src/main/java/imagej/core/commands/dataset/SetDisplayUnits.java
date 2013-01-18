cbe9d0794177655f465dbe6c4cc3586220a4da94
Barry DeZonia <bdezonia@wisc.edu>
2013-01-18 16:51:27 -0600

    Make plugins for setting Dataset and ImageDisplay units

---
+++ core/commands/data/src/main/java/imagej/core/commands/dataset/SetDisplayUnits.java
@@ -0,0 +1,27 @@
+package imagej.core.commands.dataset;
+
+import imagej.command.Command;
+import imagej.data.display.ImageDisplay;
+import imagej.plugin.Parameter;
+import imagej.plugin.Plugin;
+import net.imglib2.meta.Axes;
+
+@Plugin(menuPath = "Plugins>Sandbox>Set Display Units")
+public class SetDisplayUnits implements Command {
+
+	@Parameter
+	private ImageDisplay display;
+
+	@Parameter(label = "X Axis Display Unit")
+	private String xUnit;
+
+	@Parameter(label = "Y Axis Display Unit")
+	private String yUnit;
+
+	@Override
+	public void run() {
+		display.setUnit(Axes.X, xUnit);
+		display.setUnit(Axes.Y, yUnit);
+	}
+
+}
