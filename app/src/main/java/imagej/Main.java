9641c3badc63c5c2eb1142ede5a5d72c1ca6ff7c
Johannes Schindelin <johannes.schindelin@gmx.de>
2013-08-12 20:30:11 -0500

    TO-UNDO: more fine-grained profiling with direct javassisting
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- app/src/main/java/imagej/Main.java
+++ app/src/main/java/imagej/Main.java
@@ -37,6 +37,13 @@ package imagej;
 
 import fiji.PerformanceProfiler;
 
+import javassist.CannotCompileException;
+import javassist.ClassPool;
+import javassist.CtBehavior;
+import javassist.CtClass;
+import javassist.expr.ExprEditor;
+import javassist.expr.MethodCall;
+
 /**
  * Launches ImageJ.
  * 
@@ -48,6 +55,47 @@ public final class Main {
 		// prevent instantiation of utility class
 	}
 
+public static long cumulative, counter;
+static void patch() {
+	try {
+		ClassPool cp = ClassPool.getDefault();
+		CtClass c = cp.get("imagej.command.CommandInfo");
+		//final String millis = "java.lang.System.currentTimeMillis();";
+		final String millis = "System.nanoTime();";
+		for (CtBehavior b : c.getDeclaredConstructors()) {
+			b.insertBefore("imagej.Main.cumulative -= " + millis);
+			b.insertAfter("imagej.Main.cumulative +=  " + millis);
+		}
+		c = cp.get("imagej.command.DefaultCommandService");
+		//CtBehavior b = c.getDeclaredMethod("wrapAsCommand");
+		CtBehavior b = c.getDeclaredMethod("addCommands");
+		b.instrument(new ExprEditor() {
+			@Override
+			public void edit(MethodCall call) throws CannotCompileException {
+System.err.println("call: " + call.getMethodName());
+				if (call.getMethodName().equals("wrapAsCommand")) {
+System.err.println("Yes");
+					//call.replace("$_ = wrapAsCommand($1); imagej.Main.cumulative += " + millis);
+				}
+			}
+		});
+
+		c = cp.get("imagej.module.DefaultModuleService");
+		b = c.getDeclaredMethod("addModules");
+		b.insertBefore("imagej.Main.cumulative -= " + millis);
+		b.insertAfter("imagej.Main.cumulative += " + millis);
+
+		c = cp.get("org.scijava.plugin.PluginIndex");
+		for (CtBehavior b2 : c.getDeclaredConstructors()) {
+			b2.insertBefore("imagej.Main.cumulative -= " + millis);
+			b2.insertAfter("imagej.Main.cumulative +=  " + millis);
+		}
+
+		c.toClass();
+	} catch (Throwable t) {
+		t.printStackTrace();
+	}
+}
 	/**
 	 * Launches a new instance of ImageJ, displaying the default user interface.
 	 * <p>
@@ -63,13 +111,16 @@ public final class Main {
 	 * @return The newly launched ImageJ instance.
 	 */
 	public static ImageJ launch(final String... args) {
-PerformanceProfiler.setActive(true);
+Thread.currentThread().setContextClassLoader(Main.class.getClassLoader());
+//patch();
+long start = System.nanoTime();
+//PerformanceProfiler.setActive(true);
 		final ImageJ ij = new ImageJ();
-PerformanceProfiler.report(System.err);
+//PerformanceProfiler.report(System.err);
 long end = System.nanoTime();
 System.err.println("Cumulative: " + ((end - start)/1e6));
 System.err.println("Cumulative2: " + (cumulative/1e6));
-if (true) return null;
+//if (true) return null;
 
 		// parse command line arguments
 		ij.console().processArgs(args);
@@ -81,8 +132,8 @@ if (true) return null;
 	}
 
 	public static void main(final String... args) throws Throwable {
-if (PerformanceProfiler.startProfiling(Main.class.getName(), args)) return;
-PerformanceProfiler.setActive(false);
+//if (PerformanceProfiler.startProfiling(Main.class.getName(), args)) return;
+//PerformanceProfiler.setActive(false);
 		launch(args);
 	}
 
